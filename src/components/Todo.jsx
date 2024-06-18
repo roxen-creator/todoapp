088fa2a1c24bce84354bc40247a5b1e8a5c0dded
roexen <frankroyal41@gmail.com>
2024-06-18 13:43:25 +0545

    submit function completed

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import TodoItems from "./TodoItems";
+
 export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function todoHandle(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
   return (
-    <>
-      <h1>Todo</h1>
-    </>
+    <form onSubmit={todoHandle}>
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="submit">Add</button>
+
+      {todos.map((items) => (
+        <TodoItems key={items} items={items} />
+      ))}
+    </form>
   );
 }
---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,3 @@
+export default function TodoItems({ items }) {
+  return <h1>{items}</h1>;
+}
