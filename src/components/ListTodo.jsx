e4362606555f2961ddb5c513f3978efec5e8ac7e
roexen <frankroyal41@gmail.com>
2024-06-19 22:03:16 +0545

    sorting function added

--- src/components/ListTodo.jsx
+++ src/components/ListTodo.jsx
@@ -1,9 +1,12 @@
 import TodoItems from "./TodoItems";
 import styles from "./todolist.module.css";
 export default function ListTodo({ todos, setTodos }) {
+  const sortedTask = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className={styles.list}>
-      {todos.map((items) => (
+      {sortedTask.map((items) => (
         <TodoItems
           key={items.name}
           items={items}
